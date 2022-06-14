60e252bd340eb5cfeda580c8325257db50860b14
Bruna Girão <brunagirao.s@gmail.com>
2022-06-14 18:13:37 -0300

    hardevQuickAction - add removeCommentAPI, saveAccountComments & showToast methods

--- force-app/main/default/lwc/hardevQuickAction/hardevQuickAction.js
+++ force-app/main/default/lwc/hardevQuickAction/hardevQuickAction.js
@@ -1,6 +1,9 @@
 import { LightningElement, api, track, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { deleteRecord } from 'lightning/uiRecordApi';
 
 import loadComments from '@salesforce/apex/CommentController.loadComments';
+import saveComments from '@salesforce/apex/CommentController.saveComments';
 
 export default class HardevQuickAction extends LightningElement {
 
@@ -51,15 +54,54 @@ export default class HardevQuickAction extends LightningElement {
         this.comments = this.comments.filter(item => item.Id != commentId);
     }
 
-    changeComment(element) {
+    async removeCommentAPI(element) {
         let commentId = element.currentTarget.dataset.commentId;
         let selectComment = this.comments.find(item => item.Id == commentId);
-        selectComment.Comment = element.target.value;
+
+        if (selectComment.Type == '_database_') {
+            let response = await deleteRecord(commentId);
+            console.log(JSON.stringify(response));
+            const event = new ShowToastEvent({
+                title   : 'Success',
+                message : 'Comment removed successfully',
+                variant : 'success'
+            });
+            this.dispatchEvent(event);
+        }
+
+        this.comments = this.comments.filter(item => item.Id != commentId);
+        
+    }
+
+    changeComment(element) {
+        let commentId           = element.currentTarget.dataset.commentId;
+        let selectComment       = this.comments.find(item => item.Id == commentId);
+        selectComment.Comment   = element.target.value;
+    }
+
+    async saveAccountComments() {
+        let response = await saveComments({
+            accountId       : this.recordId,
+            commentsJSON    : JSON.stringify(this.comments),
+            removedComments : this.removedComments
+        });
+        console.log(JSON.stringify(response));
+        this.showToast(response);
+        
     }
 
     debugComments() {
         console.log(JSON.stringify(this.comments));
         console.log(JSON.stringify(this.removedComments));
+    }
+
+    showToast(response) {
+        const event = new ShowToastEvent({
+            title   : response.ToastInfo.Title,
+            message : response.ToastInfo.Message,
+            variant : response.ToastInfo.Type
+        });
+        this.dispatchEvent(event);
     }
 
 }
