dadc852ea37cfbdc0ea63123352a86458934a3bd
Bruna Girão <brunagirao.s@gmail.com>
2022-06-14 16:47:16 -0300

    hardevQuickAction - add crud methods

--- force-app/main/default/lwc/hardevQuickAction/hardevQuickAction.js
+++ force-app/main/default/lwc/hardevQuickAction/hardevQuickAction.js
@@ -6,6 +6,8 @@ export default class HardevQuickAction extends LightningElement {
 
     @api recordId;
     @track comments;
+    @track countId = 0;
+    @track removedComments = [];
 
     connectedCallback() {
         console.log('iniciando');
@@ -23,6 +25,41 @@ export default class HardevQuickAction extends LightningElement {
             this.comments = JSON.parse(response.ResponseJSON);
             console.log('Comments =>', JSON.stringify(this.comments));
         }
+    }
+
+    addNewComment() {
+        this.comments.push({
+            Comment     : '',
+            AccountId   : this.recordId,
+            Type        : '_new_',
+            Id          : this.countId,
+        });
+
+        this.countId++;
+    }
+
+    removeComment(element) {
+        let commentId = element.currentTarget.dataset.commentId;
+        let selectComment = this.comments.find(item => item.Id == commentId);
+        //let index = this.comments.findIndex(item => item.Id == commentId);
+        //this.comments.splice(index, 1);
+        
+        if (selectComment.Type == '_database_') {
+            this.removedComments.push(commentId);
+        }
+
+        this.comments = this.comments.filter(item => item.Id != commentId);
+    }
+
+    changeComment(element) {
+        let commentId = element.currentTarget.dataset.commentId;
+        let selectComment = this.comments.find(item => item.Id == commentId);
+        selectComment.Comment = element.target.value;
+    }
+
+    debugComments() {
+        console.log(JSON.stringify(this.comments));
+        console.log(JSON.stringify(this.removedComments));
     }
 
 }
